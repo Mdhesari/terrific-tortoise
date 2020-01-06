c976a9fd6a3ad495318a6f95e6fcc8e22eeb2199
Mohamad Fazel <mdhesari99@gmail.com>
2020-01-06 11:54:48 +0330

    plugin remark options

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-source-data`,
     {
-      resolve: `gatsby-remark-responsive-iframe`,
-      options: {
-        wrapperStyle: `margin-bottom: 1.0725rem`
-      }
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
@@ -43,6 +37,12 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
             }
           }
         ]
